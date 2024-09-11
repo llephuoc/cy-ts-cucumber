d62f71b6659b23fac9070073abd3c1dfeb5656f2
llephuoc <68322222+llephuoc@users.noreply.github.com>
2024-09-11 22:26:36 +0700

    Update e2e.ts

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import './commands/mark'
+import './comands/measure'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
